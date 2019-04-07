63ad0ed7fa89ed6e399b4262f60ddb935fd52d27
camilo <cortiz20@amherst.edu>
2019-04-07 16:14:07 -0400

    more methods and fields

--- FinalPage.java
+++ FinalPage.java
@@ -1,3 +1,19 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import java.util.Random;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.RenderingHints;
+import java.awt.GradientPaint;
+
 public class FinalImage{
-  
+  public static final int WIDTH=1024;
+  public static final int HEIGHT=580;
+
+  public void paintComponent(Graphics g){
+
+  }
 }
--- ImagePage.java
+++ ImagePage.java
@@ -1,3 +1,20 @@
-public class ImagePage{
-  
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import java.util.Random;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.RenderingHints;
+import java.awt.GradientPaint;
+
+public class ImagePage extends JPanel{
+  public static final int WIDTH=1024;
+  public static final int HEIGHT=580;
+
+  public void paintComponent(Graphics g){
+
+  }
+
 }
