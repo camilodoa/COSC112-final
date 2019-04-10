037dcb7e55ac7d049cf46c7a5cd2fd98c32272dc
Kelley Baumann <kelleybaumann@vlan423-242.subnet-238.amherst.edu>
2019-04-10 11:53:58 -0400

    Kelley updated greeting

--- GreetingPage.java
+++ GreetingPage.java
@@ -18,12 +18,45 @@ public class GreetingPage extends JPanel implements Page{
     this.HEIGHT = Page.HEIGHT;
     this.WIDTH = Page.WIDTH;
 
-    //set screen size
+    // set screen size
     this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
   }
 
+  @Override
   public void paintComponent(Graphics g){
+    super.paintComponent(g);
 
+    // set background color
+    g.setColor(new Color(25,25,112));
+    g.fillRect(0,0,WIDTH,HEIGHT);
+
+    // make button
+    g.setColor(new Color(124,252,0));
+    g.fillOval(WIDTH/2,HEIGHT-300,200,200);
+
+    // make description
+    g.setColor(Color.WHITE);
+    int x_text = 100;
+    int y_text = 100;
+    g.drawString("Welcome to the Face Distorter 2000 program.",x_text,y_text); 
+    g.drawString("You will first be prompted to upload an image that includes a face.",x_text,y_text+20);
+    g.drawString("Then, you will be asked to identify the location of the face,",x_text,y_text+40); 
+    g.drawString("as well as eyes and mouth, using your mouse or trackpad.",x_text,y_text+60);
+    g.drawString("Afterwards, you will be able to distort the face via a number of selections.",x_text,y_text+80);
+    g.drawString("You will then be provided with the distorted image and options to save.",x_text,y_text+100);
+
+    // first instructions
+    g.setColor(new Color(124,252,0));
+    g.drawString("To get started, click on the big green button to upload an image.",x_text,y_text+120);
+
+  }    
+
+  public static void main(String[] args) {
+    JFrame welcome = new JFrame("Face Distorter 2000");
+    welcome.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    welcome.setContentPane(new GreetingPage());
+    welcome.pack();
+    welcome.setVisible(true);
   }
 
 }
