24f0dd06b1a756604dea2ef08c7a7f32f9b7b0ba
camilo <cortiz20@amherst.edu>
2019-04-14 23:21:42 -0400

    file formatting

---
+++ src/FinalPage.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+
+public class FinalPage extends Page{
+
+  public FinalPage(){
+
+    //set screen size
+    this.setPreferredSize(new Dimension(this.WIDTH, this.HEIGHT));
+  }
+
+  public void paintComponent(Graphics g){
+
+  }
+}
